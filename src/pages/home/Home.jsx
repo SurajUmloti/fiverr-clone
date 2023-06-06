8c169cf7128d728566aab72a5966c2431c2273a7
Suraj Umloti <umloti.suraj@gmail.com>
2023-06-06 22:52:06 +0530

    Added Featured area in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,27 @@ const Home = () => {
     return (
         <div className="home">
             <div className="container">
-                Home Page
+                <div className="left-section">
+                    <div className="title">
+                        <span>Find the right <i>freelance</i> service, right away</span>
+                    </div>
+                    <div className="search-field">
+                        <div className="input">
+                            <input type="text" placeholder="Search for any service..." />
+                        </div>
+                        <button>Search</button>
+                    </div>
+                    <div className="popular-services">
+                        <span>Popular:</span>
+                        <span>Website Design</span>
+                        <span>WordPress</span>
+                        <span>Logo Design</span>
+                        <span>AI Service</span>
+                    </div>
+                </div>
+                <div className="right-section">
+                    <img src="./images/man.png" alt="" />
+                </div>
             </div>
         </div>
     )
