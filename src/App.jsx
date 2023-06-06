aab8e9d13d29299ac5c59c7c053c7c6f6390d461
Suraj Umloti <umloti.suraj@gmail.com>
2023-06-06 16:49:11 +0530

    First-Commit: fiverr-clone app initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="app">
+        <div className="container">
+          fiverr - clone App
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
